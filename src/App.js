208307779c5845d9f6faf046af63e122806a60ba
Roma Kvasha <romajan.ru@gmail.com>
2025-02-20 00:26:53 +0100

    added a section with expenses

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
-
+import Costs from "./components/Costs/Costs"
 function App() {
+  const costs = [
+    { date: new Date(2021, 2, 12), description: "Refrigerator", amount: 999 },
+    { date: new Date(2021, 3, 10), description: "Auto", amount: 10000 },
+    { date: new Date(2021, 4, 25), description: "Trousers", amount: 50 },
+  ];
+
   return (
     <div>
-     <h1>hello</h1>
+      <Costs costs={costs}/>
     </div>
   );
 }
